03ee0bf87f097047bdac2f885ad2f8812cf9619b
mikaylab12 <82205121+mikaylab12@users.noreply.github.com>
2021-04-12 16:38:13 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+# Task 1 Sample Solution
+print("Hello World!")
+
+
+# Task 2 Sample Solution
+_PI = 3.14
+radius = int(input("Please enter the radius: "))
+area = _PI * radius**2
+print("The area of the cirlce is: " + str(area))
+
+# Task 3 Sample Solution
+diameter = 2 * radius
+print("The diameter of the cirle is: " + str(diameter))
+
+
+_PI = 3.14
+circumference = _PI * radius * 2
+print("The circumference of the circle is: " + str(circumference))
